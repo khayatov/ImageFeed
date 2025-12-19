1dc73dbc6f15fe56ab3fb5e66b109f538dcb2972
Khayatov Andrey <khayatov.a@gmail.com>
2025-12-19 06:16:03 +0300

    feat(products): добавить ScrollView на экран детального просмотра фото

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -9,21 +9,88 @@ import UIKit
 
 final class SingleImageViewController: UIViewController {
     @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var scrollView: UIScrollView!
     
     @IBAction func didTapBackButton() {
         dismiss(animated: true, completion: nil)
     }
+    @IBAction func didTapShareButton() {
+        guard let image else { return }
+        let share = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        centerImage()
+    }
+    
+    private func centerImage() {
+        let imageViewSize = imageView.frame.size
+        let scrollViewSize = scrollView.bounds.size
+        
+        var verticalInset: CGFloat = 0
+        var horizontalInset: CGFloat = 0
+        
+        if imageViewSize.width < scrollViewSize.width {
+            horizontalInset = (scrollViewSize.width - imageViewSize.width) / 2
+        }
+        
+        if imageViewSize.height < scrollViewSize.height {
+            verticalInset = (scrollViewSize.height - imageViewSize.height) / 2
+        }
+
+        scrollView.contentInset = UIEdgeInsets(
+            top: verticalInset,
+            left: horizontalInset,
+            bottom: verticalInset,
+            right: horizontalInset
+        )
+    }
     
     var image: UIImage? {
-            didSet {
-                guard isViewLoaded else { return }
-                imageView.image = image
-            }
+        didSet {
+            guard isViewLoaded, let image else { return }
+            
+            imageView.image = image
+            imageView.frame.size = image.size
+            rescaleAndCenterImageInScrollView(image: image)
         }
+    }
     
     override func viewDidLoad() {
-            super.viewDidLoad()
+        super.viewDidLoad()
         
-            imageView.image = image
-        }
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
+        guard let image else { return }
+        imageView.image = image
+        imageView.frame.size = image.size
+        rescaleAndCenterImageInScrollView(image: image)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        centerImage()
+    }
 }
